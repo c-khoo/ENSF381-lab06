7044df327b78c65a2cf7e91623a43737c860a755
Chloe Khoo <chloe.hc.khoo@gmail.com>
2026-03-13 13:00:47 -0600

    Completed exercise 2 up to around page 16

--- lab06/Starter/src/exercise2/TicTacToePage.js
+++ lab06/Starter/src/exercise2/TicTacToePage.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 function TicTacToePage() {
   return (
     <>
@@ -6,8 +8,39 @@ function TicTacToePage() {
       </section>
 
       <section className="panel">
-        <div className="ttt-board">{/* Build Square, Board, and calculateWinner in this file. */}</div>
+        <div className="ttt-board">
+          {/* Build Square, Board, and calculateWinner in this file. */}
+          <Board />
+        </div>
       </section>
+    </>
+  );
+}
+
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
+      </div>
     </>
   );
 }
